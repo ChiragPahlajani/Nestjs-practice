b8d1de9af2e70aebdeaa9542411fb1f850e32b54
chirag pahlajani <chiragpahlajani43@gmail.com>
2025-11-14 01:17:30 +0530

    chore: completed with task1

--- messages-app/src/app.service.ts
+++ messages-app/src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { AppRepository } from './app.repository';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(public appRepo: AppRepository) {}
+  getHello() {
+    return this.appRepo.findAll();
+  }
+  getHelloId(id: string) {
+    return this.appRepo.findOne(id);
+  }
+  create(content: string) {
+    return this.appRepo.create(content);
   }
 }
