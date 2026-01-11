232d51b21e96176bfe54ebd2521fca194be7b6a8
chirag pahlajani <chiragpahlajani43@gmail.com>
2026-01-12 04:34:59 +0530

    fix: some more minor fixes

--- authify/src/users/user.entity.ts
+++ authify/src/users/user.entity.ts
@@ -7,7 +7,7 @@ import {
   OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
-
+import { Report } from '../reports/report.entity';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
