bf83a535fe24ff42ab13dbd30d088bebfad6bf67
chirag pahlajani <chiragpahlajani43@gmail.com>
2026-01-24 21:53:39 +0530

    chore: add endpoint for report approval

--- authify/src/reports/reports.controller.ts
+++ authify/src/reports/reports.controller.ts
@@ -1,17 +1,34 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  UseGuards,
+  Patch,
+  Param,
+} from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
+import { AuthGuard } from 'src/users/guards/auth.guard';
+import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/users/guards/admin.guard';
 @Controller('reports')
 export class ReportsController {
   constructor(private reportService: ReportsService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportService.create(body, user);
+  }
+
+  @Patch('/:id')
+  @UseGuards(AdminGuard)
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportService.changeApproval(id, body.approved);
   }
 }
