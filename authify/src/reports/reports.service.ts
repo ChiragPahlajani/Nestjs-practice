68d5caf4d0c22c465ce3dc3cbeef77bded48bf1a
chirag pahlajani <chiragpahlajani43@gmail.com>
2026-01-24 21:54:53 +0530

    chore: implement report approval service part

--- authify/src/reports/reports.service.ts
+++ authify/src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -13,6 +13,16 @@ export class ReportsService {
   create(body: CreateReportDto, user: User) {
     const report = this.reportRepository.create(body);
     report.user = user;
+    return this.reportRepository.save(report);
+  }
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.reportRepository.findOne({
+      where: { id: parseInt(id) },
+    });
+    if (!report) {
+      throw new NotFoundException('Report not found');
+    }
+    report.approved = approved;
     return this.reportRepository.save(report);
   }
 }
